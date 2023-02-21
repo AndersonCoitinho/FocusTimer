ad484483eb7cac63c061c96c74b384a0bc61e686
Anderson Coitinho <andercoitinho@gmail.com>
2023-02-20 22:13:12 -0300

    Finalizando refatoração

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
-import resetControls from "./controls.js"
-import { Timer } from "./timer.js"
-
-
+import Controls from "./controls.js"
+import Timer from "./timer.js"
 
 const buttonPlay = document.querySelector('.play')
 const buttonPause = document.querySelector('.pause')
@@ -11,37 +9,33 @@ const buttonSoundOn = document.querySelector('.sound-on')
 const buttonSoundOff = document.querySelector('.sound-off')
 const minutesDisplay = document.querySelector('.minutes')
 const secondsDisplay = document.querySelector('.seconds')
-let minutes = Number(minutesDisplay.textContent)
-let timerTimeOut
 
+const controls = Controls({
+  buttonPause,
+  buttonPlay,
+  buttonSet,
+  buttonStop
+})
 
 const timer = Timer({
   minutesDisplay, 
   secondsDisplay, 
-  timerTimeOut, 
-  resetControls,
+  resetControls: controls.reset
 })
 
 buttonPlay.addEventListener('click', function() {
-    console.log("aqui")
-  buttonPlay.classList.add('hide')
-  buttonPause.classList.remove('hide')
-  buttonSet.classList.add('hide')
-  buttonStop.classList.remove('hide')
-
+  controls.play()
   timer.countdown()
-
 })
 
 buttonPause.addEventListener('click', function() {
-  buttonPause.classList.add('hide')
-  buttonPlay.classList.remove('hide')
-  clearTimeout(timerTimeOut)
+  controls.pause()
+  timer.hold()
 })
 
 buttonStop.addEventListener('click', function() {
-  resetControls()
-  timer.resetTimer()
+  controls.reset()
+  timer.reset()
 })
 
 buttonSoundOff.addEventListener('click', function() {
@@ -55,12 +49,13 @@ buttonSoundOn.addEventListener('click', function() {
 })
 
 buttonSet.addEventListener('click', function() {
-  let newMinutes = prompt('Quantos minutos?')
+  let newMinutes = controls.getMinutes()
+
   if (!newMinutes) {
-    timer.resetTimer()
+    timer.reset()
     return
   }
 
-  minutes = newMinutes
-  updateTimerDisplay(minutes, 0)
+  timer.updateDisplay(newMinutes, 0)
+  timer.updateMinutes(newMinutes)
 })
